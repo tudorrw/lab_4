fd8e6886440c689a1d920961c1fa90f06f1e56fb
tudorrw <143077299+tudorrw@users.noreply.github.com>
2023-10-30 17:19:59 +0200

    creating lab_2 -implementation of model entities -crating packages for MVC -crating repo interfac

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.example.repo.RepositoryInMemory;
+import org.example.controller.Controller;
+import org.example.view.View;
+public class Main {
+    public static void main(String[] args) {
+//        RepositoryInMemory repo = new RepositoryInMemory<>();
+//        Controller controller = new Controller(repo);
+//        View view = new View(controller);
+//        view.run();
+        System.out.printf("Hello and welcome!");
+    }
+}
