af89e9df895195aaeeaff2a7aa2b7045c23c209b
tudorrw <143077299+tudorrw@users.noreply.github.com>
2023-11-13 21:07:38 +0200

    static method createView

--- src/main/java/org/example/view/PersonViewFactory.java
+++ src/main/java/org/example/view/PersonViewFactory.java
@@ -4,14 +4,11 @@ import org.example.controller.AdminController;
 import org.example.controller.UserController;
 
 public class PersonViewFactory {
-    public IPersonView createView(int selection, UserController userController, AdminController adminController) {
-        switch (selection) {
-            case 1:
-                return new UserView(userController);
-            case 2:
-                return new AdminView(adminController);
-            default:
-                return null;
-        }
+    public static IPersonView createView(int selection, UserController userController, AdminController adminController) {
+        return switch (selection) {
+            case 1 -> new UserView(userController);
+            case 2 -> new AdminView(adminController);
+            default -> null;
+        };
     }
 }
